7ed75cb7e3957ca1771d964084ab80cdbe03e98e
Ondrej Kvasnovsky <ondrej.kvasnovsky@gmail.com>
2020-09-14 21:37:29 -0700

    added qa_faq_combine search

--- src/search/QueryRequest.ts
+++ src/search/QueryRequest.ts
@@ -4,7 +4,14 @@ export class QueryRequest {
     query: string;
     n_best: number;
     skip?: number;
-    func_type?: 'faq' | 'qa' | 'lm' | 'qa_faq' | string;
+    /**
+     * qa - questions and answers
+     * faq - frequently asked qa
+     * lm - language model
+     * qa_faq - returns qa and faq in one request
+     * qa_faq_combine - returns qna and faq together in one list, sorted by score
+     */
+    func_type?: 'faq' | 'qa' | 'lm' | 'qa_faq' | 'qa_faq_combine' | string;
     answer_type?: 'sentence' | 'document' | 'phrase';
     filters?: {
       exists?: { [key: string]: any };
